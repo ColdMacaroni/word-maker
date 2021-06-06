03bf2baf7863b2cc9782336199fdde4f3cbf93b8
muffin <squishysparrow7.7@gmail.com>
2021-06-06 23:43:05 +1200

    start on the python file for generating a nn

---
+++ network_maker.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# network_maker.py
+# Why shouldn't i try to write a NN from scratch, huh?
+# parser.py Copyright (C) 2021 Sof
+# ^The GPL3 told me to put that there <shrug>
+
+# For sigmoid
+from math import exp
+
+# Here goes nothing!
+class Neuron:
+    def __init__():
+        pass
+
+def sigmoid(x):
+    """
+    Sigmoid function
+    """
+    # f(x) = 1/(1 + e^(-x))
+    return 1/(1 + exp(-abs(x)))
+
+if __name__ == "__main__":
+    print("Try importing instead!")
+
