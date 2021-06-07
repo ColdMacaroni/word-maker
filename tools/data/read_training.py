4c619c4fb163db23fa0049284abff0dd43dbea34
muffin <squishysparrow7.7@gmail.com>
2021-06-08 09:26:15 +1200

    Start on reader fro training files

---
+++ tools/data/read_training.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# read_training.py
+# A script to read the data from the parser
+# parser.py Copyright (C) 2021 Sof
+# ^The GPL3 told me to put that there <shrug>
+
+def main(filename):
+    """
+    Returns a list wit:
+    """
+
+if __name__ == "__main__":
+    from sys import argv
+    main(argv[1])
