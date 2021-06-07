a92284697b9fe1c5f3d00dfeeffd528dec6a1880
muffin <squishysparrow7.7@gmail.com>
2021-06-07 13:24:39 +1200

    translate chars into normalized number for the nn

--- tools/parser.py
+++ tools/parser.py
@@ -45,16 +45,42 @@ def pad_right(string, length, pad_char=' '):
     return new_string
 
 
+def translator(string, t_dict):
+    """
+    Translates strings into a normalized numbers for the NN
+    "and             " -> "0, 0.5, 0.11538461538461539, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1"
+    'a' = 0                   == (ord('a') - ord('a')) / 26
+    'n' = 0.5                 == (ord('n') - ord('a')) / 26
+    'd' = 0.11538461538461539 == (ord('d') - ord('a')) / 26
+    ' ' = 1
+
+    :param string: The string to be translated
+    :param t_dict: Dictionary to follow for translations
+    """
+    new_chars = list()
+
+    for char in string:
+        # Have to convert into str to be able to use .join
+        new_chars.append(str(t_dict[char.lower()]))
+
+    return ','.join(new_chars)
+
+
 def main(raw_folder, parsed_folder, str_len=16, sep_char=';'):
     """
     Parses data from all text files in the raw folder into a format of
     string{sep_char}length
     e.g. (when size=16 and sep_char=';'):
         Input file:
-            "Macaroni\n"
+            "Macaroni\\n"
 
-        Output file:
+        Turns into:
             "Macaroni        ;8"
+
+        Output file (Only one newline):
+            "0.46153846153846156, 0.0, 0.07692307692307693,
+             0.0, 0.6538461538461539, 0.5384615384615384,
+             0.5, 0.3076923076923077, 1, 1, 1, 1, 1, 1, 1, 1;8\\n"
 
     :param raw_folder:
     :param parsed_folder:
@@ -66,6 +92,19 @@ def main(raw_folder, parsed_folder, str_len=16, sep_char=';'):
     # with the same filename
     input_path = Path(raw_folder)
     output_path = Path(parsed_folder)
+
+    # Generate dictionary for translations.
+    # Each letter is a normalized number between 0 and 26,
+    # 26 being a space.
+    # 26 is not a magic #, its the # of letters in the alphabet. ok?
+    tr_dict = dict()
+    for i in range(0, 26):
+        # This will go a-z
+        letter = chr(ord('a') + i)
+
+        tr_dict[letter] = i/26
+
+    tr_dict[' '] = 1.0
 
     # Make sure we get actual files
     raw_files = [f for f in input_path.iterdir() if f.is_file()]
@@ -85,9 +124,11 @@ def main(raw_folder, parsed_folder, str_len=16, sep_char=';'):
             pad_right(word, str_len, pad_char=' ') for word in words
         ]
 
+        tr_words = [translator(word, tr_dict) for word in padded_words]
+
         # Put the lengths and padded words together for easier writing
         # I couldnt think of a better name
-        words_n_lens = zip(padded_words, word_lens)
+        words_n_lens = zip(tr_words, word_lens)
 
         # I'm not sure how the / works but this is the same as saying
         # output_file = Path(f"{output_path}/{file.name}")
